a2f2b9c5154770afbc8d740842116ac51bd09c23
Kyrylo Silin <silin@kyrylo.org>
2016-06-25 15:49:58 +0300

    Improve documentation

--- README.md
+++ README.md
@@ -48,7 +48,6 @@ Examples
 This is the minimal example that you can use to test Airbrake Rust with your
 project:
 
-
 ```rust
 extern crate airbrake;
 
@@ -66,13 +65,82 @@ fn main() {
 
     match double_number("NOT A NUMBER") {
         Ok(n) => assert_eq!(n, 20),
+        // Asynchronously sends the error to the dashboard.
         Err(err) => airbrake.notify(err),
     }
 
+    // Joins worker threads.
     airbrake.close();
 }
+```
+
+Configuration
+-------------
+
+### project_key & project_id
+
+You **must** set both `project_id` & `project_key`.
+
+To find your `project_id` and `project_key` navigate to your project's _General
+Settings_ and copy the values from the right sidebar.
+
+![][project-idkey]
+
+```rust
+let mut airbrake = airbrake::configure(|config| {
+    config.project_id = "113743".to_owned();
+    config.project_key = "81bbff95d52f8856c770bb39e827f3f6".to_owned();
+});
+```
+
+### host
+
+By default, it is set to `https://airbrake.io`. A `host` is a web address
+containing a scheme ("http" or "https"), a host and a port. You can omit the
+port (80 will be assumed).
+
+```rust
+let mut airbrake = airbrake::configure(|config| {
+    config.host = "http://localhost:8080".to_owned();
+});
+```
+
+### workers
+
+The number of threads that handle notice sending. The default value is 1.
+
+```rust
+let mut airbrake = airbrake::configure(|config| {
+    config.workers = 5;
+});
+```
+
+API
+---
+
+### airbrake
+
+#### airbrake.notify
+
+Sends an error to Airbrake *asynchronously*. `error` must implement the
+[`std::error::Error`][stderror] trait. Returns `()`.
+
+```rust
+airbrake.notify(error);
+```
+
+#### airbrake.notify_sync
+
+Sends an error to Airbrake *synchronously*. `error` must implement the
+[`std::error::Error`][stderror] trait. Returns `?????????`. Accepts the same
+parameters as [`Airbrake.notify`](#airbrakenotify).
+
+```rust
+airbrake.notify_sync(error);
 ```
 
 [airbrake.io]: https://airbrake.io
 [notice-v3]: https://airbrake.io/docs/#create-notice-v3
 [env_logger]: https://crates.io/crates/env_logger
+[project-idkey]: https://s3.amazonaws.com/airbrake-github-assets/airbrake-ruby/project-id-key.png
+[stderror]: https://doc.rust-lang.org/std/error
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-//! Airbrake Rust is notifier library for the Rust Programming language. The
-//! library provides minimalist API that enables the ability to send Rust errors
-//! to the Airbrake dashboard.
+//! Airbrake Rust is an [Airbrake][airbrake.io] notifier library for the Rust
+//! Programming language. The library provides minimalist API that enables the
+//! ability to send Rust errors to the Airbrake dashboard.
 //!
-//! # Installation
+//! Installation
+//! ------------
 //!
 //! Add the crate to your Cargo.toml:
 //!
@@ -11,14 +12,15 @@
 //! airbrake = "0.1"
 //! ```
 //!
-//! # Examples
+//! Examples
+//! --------
 //!
 //! ### Basic example
 //!
 //! This is the minimal example that you can use to test Airbrake Rust with your
 //! project:
 //!
-//! ```rust
+//! ```
 //! extern crate airbrake;
 //!
 //! use std::num::ParseIntError;
@@ -35,12 +37,89 @@
 //!
 //!     match double_number("NOT A NUMBER") {
 //!         Ok(n) => assert_eq!(n, 20),
+//!         // Asynchronously sends the error to the dashboard.
 //!         Err(err) => airbrake.notify(err)
 //!     }
 //!
+//!     // Joins worker threads.
 //!     airbrake.close();
 //! }
 //! ```
+//!
+//! Configuration
+//! -------------
+//!
+//! ### project_key & project_id
+//!
+//! You **must** set both `project_id` & `project_key`.
+//!
+//! To find your `project_id` and `project_key` navigate to your project's _General
+//! Settings_ and copy the values from the right sidebar.
+//!
+//! ![][project-idkey]
+//!
+//! ```rust
+//! let mut airbrake = airbrake::configure(|config| {
+//!     config.project_id = "113743".to_owned();
+//!     config.project_key = "81bbff95d52f8856c770bb39e827f3f6".to_owned();
+//! });
+//! ```
+//!
+//! ### host
+//!
+//! By default, it is set to `https://airbrake.io`. A `host` is a web address
+//! containing a scheme ("http" or "https"), a host and a port. You can omit the
+//! port (80 will be assumed).
+//!
+//! ```rust
+//! let mut airbrake = airbrake::configure(|config| {
+//!     config.host = "http://localhost:8080".to_owned();
+//! });
+//! ```
+//!
+//! ### workers
+//!
+//! The number of threads that handle notice sending. The default value is 1.
+//!
+//! ```
+//! let mut airbrake = airbrake::configure(|config| {
+//!     config.workers = 5;
+//! });
+//! ```
+//!
+//! API
+//! ---
+//!
+//! ## airbrake
+//!
+//! #### airbrake.notify
+//!
+//! Sends an error to Airbrake *asynchronously*. `error` must implement the
+//! [`std::error::Error`][stderror] trait. Returns `()`.
+//!
+//! ```
+//! airbrake.notify(error);
+//! ```
+//!
+//! As the second parameter, accepts a hash with additional data. That data will be
+//! displayed in the _Params_ tab in your project's dashboard.
+//!
+//! #### airbrake.notify_sync
+//!
+//! Sends an error to Airbrake *synchronously*. `error` must implement the
+//! [`std::error::Error`][stderror] trait. Returns `?????????`. Accepts the same
+//! parameters as [`Airbrake.notify`](#airbrakenotify).
+//!
+//! ```
+//! airbrake.notify_sync(error);
+//! ```
+//!
+//! [airbrake.io]: https://airbrake.io
+//! [notice-v3]: https://airbrake.io/docs/#create-notice-v3
+//! [env_logger]: https://crates.io/crates/env_logger
+//! [project-idkey]: https://s3.amazonaws.com/airbrake-github-assets/airbrake-ruby/project-id-key.png
+//! [stderror]: https://doc.rust-lang.org/std/error
+
 
 extern crate hyper;
 extern crate rustc_serialize;
