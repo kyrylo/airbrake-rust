76879a5e926240df5b4d95ed4d098c56ff7b4363
Kyrylo Silin <silin@kyrylo.org>
2018-07-02 15:58:21 +0800

    Fix tests

--- tests/airbrake.rs
+++ tests/airbrake.rs
@@ -6,7 +6,7 @@ use backtrace::Backtrace;
 use mockito::{mock, Matcher};
 
 #[test]
-fn it_notifies_airbrake() {
+fn test_airbrake_notify() {
     let notifier = airbrake::Notifier::new(airbrake::Config {
         project_id: 113743,
         project_key: "81bbff95d52f8856c770bb39e827f3f6",
@@ -18,13 +18,14 @@ fn it_notifies_airbrake() {
     let notice = notifier.build_notice(error);
     let mock = mock("POST", "/api/v3/projects/113743/notices")
         .match_header("content-type", "application/json")
+        .match_header("authorization", "Bearer 81bbff95d52f8856c770bb39e827f3f6")
         .create();
     notifier.notify(notice).expect("notifier.notify failed");
     mock.assert();
 }
 
 #[test]
-fn it_attaches_a_backtrace() {
+fn test_backtrace_attachment() {
     let backtrace = Backtrace::new();
 
     let notifier = airbrake::Notifier::new(airbrake::Config {
@@ -47,7 +48,7 @@ fn it_attaches_a_backtrace() {
 }
 
 #[test]
-fn it_attaches_params() {
+fn test_params_attachment() {
     let notifier = airbrake::Notifier::new(airbrake::Config {
         project_id: 113743,
         project_key: "81bbff95d52f8856c770bb39e827f3f6",
@@ -58,38 +59,31 @@ fn it_attaches_params() {
     let error = "xc".parse::<u32>().err().unwrap();
     let mut params = std::collections::HashMap::new();
     params.insert(String::from("mango"), airbrake::Param::Int32(42));
-    params.insert(
-        String::from("banana"),
-        airbrake::Param::String(String::from("tasty")),
-    );
-
     let notice = notifier.build_notice(error).set_params(params);
 
     let mock = mock("POST", "/api/v3/projects/113743/notices")
         .match_body(Matcher::Regex(String::from(
-            r#""params":\{"banana":\{"String":"tasty"\},"mango":\{"Int32":42\}\}"#,
+            r#""params":\{"mango":\{"Int32":42\}\}"#,
         )))
         .create();
     notifier.notify(notice).expect("notifier.notify failed");
     mock.assert();
 }
 
-// #[test]
-// fn it_routes_notices_through_a_proxy() {
-//     let notifier = airbrake::Notifier::new(airbrake::Config {
-//         project_id: 113743,
-//         project_key: "81bbff95d52f8856c770bb39e827f3f6",
-//         proxy_url: "http://localhost:8080",
-//         host: mockito::SERVER_URL,
-//         ..Default::default()
-//     });
+#[test]
+#[ignore]
+fn test_routing_notices_through_a_proxy() {
+    let notifier = airbrake::Notifier::new(airbrake::Config {
+        project_id: 113743,
+        project_key: "81bbff95d52f8856c770bb39e827f3f6",
+        proxy_url: "http://127.0.0.1:8080",
+        host: mockito::SERVER_URL,
+        ..Default::default()
+    });
 
-//     let error = "xc".parse::<u32>().err().unwrap();
-//     let notice = notifier.build_notice(error);
-//     let mock = mock("POST", "/api/v3/projects/113743/notices")
-//         .with_header("proxy-authenticate", "basic")
-//         .create();
-//     let resp = notifier.notify(notice).expect("notifier.notify failed");
-//     println!("{:#?}", resp);
-//     mock.assert();
-// }
+    let error = "xc".parse::<u32>().err().unwrap();
+    let notice = notifier.build_notice(error);
+    let mock = mock("POST", "/api/v3/projects/113743/notices").create();
+    let resp = notifier.notify(notice).expect("notifier.notify failed");
+    mock.assert();
+}
